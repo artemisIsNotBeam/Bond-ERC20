78fb9cb344eed9c2ada4d1a6f5a5a9d8f27c81cf
artemisIsNotBeam <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-02-27 07:25:45 -0800

    Add files via upload

--- contractIntergration.js
+++ contractIntergration.js
@@ -1,10 +1,30 @@
-const claimbutton =  document.getElementById("claimMula");
-const addOweButton = document.getElementById("debtUpdate");
+ /*
+        https://docs.bscscan.com/api-endpoints/tokens#get-token-holder-list-by-contract-address
+        gonna use this one: Get BEP-20 Token Account Balance by ContractAddress
+        will be having that entered
+        at position of the adreess
 
-claimbutton.onclick(function(){
-    alert("claiming lol");
-})
+        the thing added will be 
+        amount*ethPerCoin
 
-addOweButton.onclick(function(){
-    alert("updating debt");
-})
+        can also set a minmium amount to claim
+        */
+
+
+//params needed: amount, wallet
+/* https://api.bscscan.com/api
+   ?module=account
+   &action=tokenbalance
+   &contractaddress=0xe9e7cea3dedca5984780bafc599bd69add087d56
+   &address=0x89e73303049ee32919903c09e8de5629b84f59eb
+   &tag=latest
+   &apikey=YourApiKeyToken
+
+//from that api you need 
+
+let api = blahblahblah
+
+you need to enter api["result"]
+for wallet api you ned to enter what you searched for
+*/
+
--- reward in ETH.sol
+++ reward in ETH.sol
@@ -9,10 +9,14 @@ import "token/ERC20/ERC20.sol";
 contract BondToken is ERC20 {
     mapping (address => uint) owe; 
     uint256 Supply = 20;
+    //pay day logic
     uint256 PayDayDelay = 30 seconds;
     uint256 LastPayday = 0 seconds;
     uint256 amountForPayDay = 30; 
     uint256 ethPerCoin = .1;
+    //minium
+    uint256 miniumAmount = 0;
+    uint256 miniumWithdraw = 0;
 
     event Withdraw(address reciever, uint amount);
     event Deposit(address sender, uint amount);
@@ -21,10 +25,9 @@ contract BondToken is ERC20 {
         _mint(msg.sender, Supply);
     }    
     //now - PayDayDelay < LastPayday;
-    function addOwe(apiReuslts{}) external{
-        uint256 give2 = owe[msg.sender];
+    function addOwe(uint amount, address theirWallet) external{
         require(now-PayDayDealy < LastPayDay);
-
+        require(msg.sender == theirWallet);
         /*
         https://docs.bscscan.com/api-endpoints/tokens#get-token-holder-list-by-contract-address
         gonna use this one: Get BEP-20 Token Account Balance by ContractAddress
@@ -40,17 +43,28 @@ contract BondToken is ERC20 {
         // this is gonna best test script so I know I have the function working
         //owe[msg.sender] = apiResults["result"]
         //that will result in a string will need to convert into number
+
+        //Just for context we pretend we have these amounts already given to us
+        require(amount > miniumAmount);
+        owe[theirWallet] += amount * ethPerCoin;
+        // also need to move date
+        
     }
 
     // from  https://ethereum.stackexchange.com/questions/77750/sending-money-through-a-smart-contract
     function withdrawAll() public {
+        //set how much we owe them 
         uint amount = owe[msg.sender];
-        require(owe[msg.sender] >= amount, "Insufficient funds");
+        //make sure their withdraw is valid
+        require(amount > miniumWithdraw);
+        //withdraw to them
         emit Withdrawal(msg.sender, amount);
+        // take away the debt to them
         owe[msg.sender] -= amount;
     }
 
     function deposositIncontract(uint256 depositAmount) public payable{
+        // just put money in
         require(msg.value >= depositAmount);
         Deposit(msg.sender, depositAmount);
     }
