33da6601cdc94237116ad02d1209ddfafa8a7689
artemisIsNotBeam <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-03-02 06:09:12 -0800

    Add files via upload

--- reward in ETH.sol
+++ reward in ETH.sol
@@ -1,58 +1,64 @@
-// contracts/GLDToken.sol
+// reward in ETH.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
+// import ERC20 functionality
 import "token/ERC20/ERC20.sol";
-
+import "token/ERC20/IERC20.sol";
 //feel free to change any of the constructor code
 
 contract BondToken is ERC20 {
     mapping (address => uint) owe; 
-    uint256 Supply = 20;
+    mapping (address => address) walletList;
+    // all these variable you can change
+    uint256 SupplyLeft = 300;
+    uint256 ICOprice=.01;
+    uint256 initialMintAMount = 20;
     //pay day logic
     uint256 PayDayDelay = 30 seconds;
     uint256 LastPayday = 0 seconds;
     uint256 amountForPayDay = 30; 
-    uint256 ethPerCoin = .1;
-    //minium
+    // thingy /more variables
+    uint256 ethPerCoin = .1;              
     uint256 miniumAmount = 0;
-    uint256 miniumWithdraw = 0;
+    uint256 miniumWithdraw = 0;          
 
-    event Withdraw(address reciever, uint amount);
+    event Withdraw(address reciever, uint amount);              
     event Deposit(address sender, uint amount);
-
-    constructor(Supply) public ERC20("BOND", "BND") {
-        _mint(msg.sender, Supply);
+    //if using this contract feel free to change any a name or ammount
+    constructor() public ERC20("BOND token", "BND") {
+        //this dosen't mint all only the initial amount
+        _mint(msg.sender, initialMintAMount);
+        SupplyLeft-= initialMintAMount;
     }    
+
+
+    function amountTheyOwen(address person) public returns(uint){
+        IERC20(person).balanceOf(account);
+    }
+
     //now - PayDayDelay < LastPayday;
-    function addOwe(uint amount, address theirWallet) external{
+    function addOwe(uint payNow) external{
         require(now-PayDayDealy < LastPayDay);
-        require(msg.sender == theirWallet);
+        require(msg.sender);
+        
         /*
-        https://docs.bscscan.com/api-endpoints/tokens#get-token-holder-list-by-contract-address
-        gonna use this one: Get BEP-20 Token Account Balance by ContractAddress
-        will be having that entered
-        at position of the adreess
+        logic for thing
 
-        the thing added will be 
-        amount*ethPerCoin
-
-        can also set a minmium amount to claim
-        */
-
-        // this is gonna best test script so I know I have the function working
-        //owe[msg.sender] = apiResults["result"]
-        //that will result in a string will need to convert into number
-
-        //Just for context we pretend we have these amounts already given to us
         require(amount > miniumAmount);
         owe[theirWallet] += amount * ethPerCoin;
-        // also need to move date
+        */
+
+        
+        //if they want to get money now call Withdraw all
+        if (payNow == true){
+            withdrawAll();
+        }
         
     }
 
+
     // from  https://ethereum.stackexchange.com/questions/77750/sending-money-through-a-smart-contract
-    function withdrawAll() public {
+    function withdrawAll() external{
         //set how much we owe them 
         uint amount = owe[msg.sender];
         //make sure their withdraw is valid
@@ -68,4 +74,18 @@ contract BondToken is ERC20 {
         require(msg.value >= depositAmount);
         Deposit(msg.sender, depositAmount);
     }
+
+    function buyBondWithEth(uint256 Howmany) public payable{
+        uint price = Howmany * ICOprice;
+        //find  price
+        require(msg.value >= price);
+        require(Howmany <= SupplyLeft);
+        //make sure the persron has enough in their wallet and we have enough left
+        _mint(msg.sender, Howmany);
+        Deposit(msg.sender, price);
+        //give us the MULA and mint them coins
+        SupplyLeft -= Howmany;
+    }
+
+    
 }
