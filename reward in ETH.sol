396d3302219479b1b0bb9c134ce72ae195a380a8
artemisIsNotBeam <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-03-02 17:10:54 -0800

    folder time

--- reward in ETH.sol
+++ reward in ETH.sol
@@ -7,21 +7,10 @@ import "token/ERC20/IERC20.sol";
 //feel free to change any of the constructor code
 
 contract BondToken is ERC20 {
-    //arrays for adding owe
-    mapping (address => uint) owe; 
-    uint walletList = [];
     // all these variable you can change
     uint256 SupplyLeft = 300;
     uint256 ICOprice=.01;
-    uint256 initialMintAMount = 20;
-    //pay day logic
-    uint256 PayDayDelay = 30 seconds;
-    uint256 LastPayday = 0 seconds;
-    uint256 amountForPayDay = 30; 
-    // thingy /more variables
-    uint256 ethPerCoin = .1;              
-    uint256 miniumAmount = 0;
-    uint256 miniumWithdraw = 0;          
+    uint256 initialMintAMount = 20;         
 
     event Withdraw(address reciever, uint amount);              
     event Deposit(address sender, uint amount);
@@ -31,57 +20,6 @@ contract BondToken is ERC20 {
         _mint(msg.sender, initialMintAMount);
         SupplyLeft-= initialMintAMount;
     }    
-
-
-    function amountTheyOwen(address person) public returns(uint){
-        return IERC20(tokenAdress).balanceOf(person);
-    }
-
-
-    function registerWallet() public{
-        uint TheyOwn = amountTheyOwen(msg.sender);
-        if (TheyOwn > miniumAmount){
-            walletList.push(msg.sender);
-        }
-    }
-    //now - PayDayDelay < LastPayday;
-    function addOwe(uint payNow) external{
-        require(now-PayDayDealy < LastPayDay);
-        require(msg.sender);
-        
-        /*
-        logic for thing
-
-        require(amount > miniumAmount);
-        owe[theirWallet] += amount * ethPerCoin;
-        */
-
-        
-        //if they want to get money now call Withdraw all
-        if (payNow == true){
-            withdrawAll();
-        }
-        
-    }
-
-
-    // from  https://ethereum.stackexchange.com/questions/77750/sending-money-through-a-smart-contract
-    function withdrawAll() external{
-        //set how much we owe them 
-        uint amount = owe[msg.sender];
-        //make sure their withdraw is valid
-        require(amount > miniumWithdraw);
-        //withdraw to them
-        emit Withdrawal(msg.sender, amount);
-        // take away the debt to them
-        owe[msg.sender] -= amount;
-    }
-
-    function deposositIncontract(uint256 depositAmount) public payable{
-        // just put money in
-        require(msg.value >= depositAmount);
-        Deposit(msg.sender, depositAmount);
-    }
 
     function buyBondWithEth(uint256 Howmany) public payable{
         uint price = Howmany * ICOprice;
