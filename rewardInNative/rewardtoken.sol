f10a8793c95ff0c079fb6dba2f62f974dfd0dc99
artemisIsNotBeam <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-03-06 06:56:55 -0800

    Add files via upload

--- rewardInNative/rewardtoken.sol
+++ rewardInNative/rewardtoken.sol
@@ -2,69 +2,72 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 // import ERC20 functionality
-import "token/ERC20/ERC20.sol";
-import "token/ERC20/IERC20.sol";
+import "../token/ERC20/ERC20.sol";
+import "../token/ERC20/IERC20.sol";
 //feel free to change any of the constructor code
 
+
+interface IBEP20 {
+    // mind the `view` modifier
+    function balanceOf(address _owner) external view returns (uint256);
+}
+
 contract RewardToken is ERC20{
-    //Important put the RewardInNative first
-    address BondContract = // IMPORTANT YOU WILL NEED TO FILL THIS OUT
-    ///////////////////////
-
-
-
-
     //arrays for adding owe
     mapping (address => uint) owe; 
-    uint walletList = [];
-    uint256 secondSupply = 10;
+    address[] WalletList;
+    // all these variable you can change
     //pay day logic
     uint256 PayDayDelay = 30 seconds;
     uint256 LastPayday = 0 seconds;
     uint256 amountForPayDay = 30; 
     // thingy /more variables
-    uint256 ethPerCoin = .1;              
+    uint256 NativePerCoin = 1;              
     uint256 miniumAmount = 0;
-    uint256 miniumWithdraw = 0;    
+    uint256 miniumWithdraw = 0;   
+    address BondTokenAdress = 0xa94E3320973399eF276a73471974535AD1F4DC3D;
+    //thats my ETh wallet send me eTH :))))
 
     event Withdraw(address reciever, uint amount);              
     event Deposit(address sender, uint amount);
-    
-    constructor() public ERC20("RewardToken", "REW") {
+
+    constructor() ERC20("RewardToken", "REW") {
         // we don't mint anything initally
     }
 
-    function amountTheyOwen(address user) public returns(uint){
+    function amountTheyOwen(address guy) public view returns(uint256){
         // first, initialize ERC20 interface instance with a token address
-        ERC20 token = ERC20(tokenAddress);
-        uint256 balance = token.balanceOf[user];
-        return balance;
+        // creating a pointer to the WBNB contract
+        IBEP20 WBNBContract = IBEP20(BondTokenAdress);
+
+        // getting balance of `bankAddress` on the WBNB contract
+        return WBNBContract.balanceOf(guy);
     }
 
 
     function registerWallet() public{
         uint TheyOwn = amountTheyOwen(msg.sender);
         if (TheyOwn > miniumAmount){
-            walletList.push(msg.sender);
+            WalletList.push(msg.sender);
         }
     }
     //now - PayDayDelay < LastPayday;
-    function addOwe(uint payNow) external{
-        require(now-PayDayDealy < LastPayDay);
-        require(msg.sender);
-        
-        /*
-        logic for thing
-
-        require(amount > miniumAmount);
-        owe[theirWallet] += amount * ethPerCoin;
-        */
-
-        
-        //if they want to get money now call Withdraw all
-
-        for (let i=0;i<walletList.length;i++){
-            address wallet = walletList[i];
+    function addOwe(bool payNow) external{
+        // make sure we can add
+        require(block.timestamp-PayDayDelay < LastPayday);
+        //update the last pay day time
+        LastPayday +=PayDayDelay;
+        //loop through all in WalletList
+        for (uint i=0;i<WalletList.length;i++){
+            //declare vars
+            address daWallet = WalletList[i];
+            uint amount = amountTheyOwen(daWallet);
+            //if they own more than minium
+            if (amount > miniumAmount){
+                //add money to their owe
+                //gives 1/10
+                owe[daWallet] += amount * NativePerCoin/ 10;
+            }
         }
         if (payNow == true){
             withdrawAll();
@@ -74,13 +77,14 @@ contract RewardToken is ERC20{
 
 
     // from  https://ethereum.stackexchange.com/questions/77750/sending-money-through-a-smart-contract
-    function withdrawAll() external{
+    function withdrawAll() public{
+        //make sure I have enough to pay out
         //set how much we owe them 
         uint amount = owe[msg.sender];
         //make sure their withdraw is valid
         require(amount > miniumWithdraw);
         //withdraw to them
-        emit Withdrawal(msg.sender, amount);
+        _mint(msg.sender,amount);
         // take away the debt to them
         owe[msg.sender] -= amount;
     }
@@ -88,6 +92,6 @@ contract RewardToken is ERC20{
     function deposositIncontract(uint256 depositAmount) public payable{
         // just put money in
         require(msg.value >= depositAmount);
-        Deposit(msg.sender, depositAmount);
+        emit Deposit(msg.sender, depositAmount);
     }
 }
